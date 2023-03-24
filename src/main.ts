083e851a7df5e5e583a91c8bb912c474b58850da
XyanZhang <1743067175@qq.com>
2023-03-24 11:30:24 +0800

    feat: 引入fastify 开启跨域

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule, 
+    new FastifyAdapter() // 使用fastify代替默认的express，以提高应用的http响应速度。
+  );
+
+  // 允许跨域
+  app.enableCors();
+  
   await app.listen(4321);
 }
 bootstrap();
